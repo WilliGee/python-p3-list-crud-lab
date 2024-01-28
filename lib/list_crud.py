2a59430cde71d758d06558c46137b2c5f804074f
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2024-01-28 18:44:06 +0300

    Completed Assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,30 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ["chest", "shoulders", "squats", "arms"]
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(data, element):
+    data.append(element)
+    return (data)
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(data, element):
+    data.insert(0,element)
+    return (data)
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(data):
+    data.pop()
+    return (data)
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(data):
+    del data[0]
+    return data
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(data):
+    return data[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(data, index):
+    return data[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(data):
+    return data[-1]
